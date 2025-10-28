5aea451857d4376aa84e28e9c6152f863de7f04e
DavidKal29 <davidkalmuk65@gmail.com>
2025-10-28 11:45:35 +0100

    Agregamos cookies a las rutas de login y register. Al terminar el proceso, se enviará una cookie al frontend con el token que contiene el id del usuario, para que este sea procesado por los middlewares que permitirán pasar a la ruta de perfil y demás rutas que requieran login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,48 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { RegisterDTO } from './dto/register.dto';
 import { AuthService } from './auth.service';
 import { LoginDTO } from './dto/login.dto';
+import type { Response } from 'express';
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+import { cookieOptions } from '../../src/cookieOptions/cookieOptions.js';
+
+dotenv.config()
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService){}
 
     @Post('register')
-    register(@Body() dto:RegisterDTO){
-        return this.authService.register(dto)
+    async register(@Body() dto:RegisterDTO, @Res() res:Response){
+        const result = await  this.authService.register(dto)
+
+        if (result.error) {
+            return res.json(result)
+        }
+
+        const token = jwt.sign({userID:result.userID},process.env.JWT_SECRET)
+
+        res.cookie('token',token,cookieOptions)
+
+        return res.json(result)
     }
 
 
     @Post('login')
-    login(@Body() dto:LoginDTO){
-        return this.authService.login(dto)
+    async login(@Body() dto:LoginDTO, @Res() res:Response){
+        const result = await  this.authService.login(dto)
+
+        if (result.error) {
+            return res.json(result)
+        }
+
+        const token = jwt.sign({userID:result.userID},process.env.JWT_SECRET)
+
+        res.cookie('token',token,cookieOptions)
+
+        return res.json(result)
+        
     }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import dotenv from "dotenv"
+import cookieParser from 'cookie-parser'
 dotenv.config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe())
+
+  app.use(cookieParser());
 
   app.enableCors({
     origin:process.env.FRONTEND_URL,
